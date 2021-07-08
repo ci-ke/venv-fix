25f482f35742ae1779acc397dbd9197ba2d61235
cike <14360436+ci-ke@users.noreply.github.com>
2021-07-08 17:44:27 +0800

    bug fix

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ python venvrelocate.py <VENV_PATH> (modified_python_name)
 
 ## Example:
 
-After you move or rename virtualenv D:\venvs\venv1 to D:\venvs\venv2, the activate scipts and executable files in venv2\scripts should not work.
+After you move or rename virtual environment D:\venvs\venv1 to D:\venvs\venv2, the activate scripts and executable files in venv2\scripts should not work.
 
 Then you can execute "python venvrelocate.py D:\venvs\venv2".
 
@@ -16,4 +16,4 @@ And then venv2 will work properly, you can activate it and use pip as usual.
 
 ## Notice:
 
-If there is a modified python name in the venv\scripts such as "python3.exe", "python2.exe", or "python36.exe"... , you should also send this name to arguments. (e.g. python venvrelocate.py venv1 python36.exe).
+If there is a modified python name in the venv\scripts such as "python3.exe" or "python36.exe"... , you should also send this name to arguments. (e.g. python venvrelocate.py venv1 python36.exe).
--- venvrelocate.py
+++ venvrelocate.py
@@ -13,18 +13,20 @@ def get_fullpath(path):
 
 
 def fix_activate(targetenv_path):
+    env_name = targetenv_path.split('\\')[-1]
     activate_path = targetenv_path+'\\Scripts\\activate'
     modified_activate_path = targetenv_path+'\\Scripts\\activate.tmp'
     if not os.path.exists(activate_path):
         return
     activate_file = open(activate_path, 'r')
     modified_activate_file = open(modified_activate_path, 'w')
-    linux_targetenv_path = targetenv_path.replace('\\', '/')
-    linux_targetenv_path = '/'+linux_targetenv_path
-    linux_targetenv_path = linux_targetenv_path.replace(':', '')
     for line in activate_file:
         if 'VIRTUAL_ENV=\"' in line:
             line = 'VIRTUAL_ENV=\"'+targetenv_path+'\"\n'
+        if ') \" != x ] ; then' in line:
+            line = '    if [ \"x('+env_name+') \" != x ] ; then\n'
+        if ') ${PS1:-}\"' in line:
+            line = '    PS1=\"('+env_name+') ${PS1:-}\"\n'
         modified_activate_file.write(line)
     activate_file.close()
     modified_activate_file.close()
@@ -42,10 +44,10 @@ def fix_activate_bat(targetenv_path):
     activate_file = open(activate_path, 'r')
     modified_activate_file = open(modified_activate_path, 'w')
     for line in activate_file:
-        if "set \"VIRTUAL_ENV=" in line:
-            line = "set \"VIRTUAL_ENV="+targetenv_path+'\"\n'
-        if "set \"PROMPT=(" in line:
-            line = "set \"PROMPT=("+env_name+") %PROMPT%\"\n"
+        if 'set VIRTUAL_ENV=' in line:
+            line = 'set VIRTUAL_ENV='+targetenv_path+'\n'
+        if 'set PROMPT=(' in line:
+            line = 'set PROMPT=('+env_name+') %PROMPT%\n'
         modified_activate_file.write(line)
     activate_file.close()
     modified_activate_file.close()
@@ -63,11 +65,10 @@ def fix_activate_ps1(targetenv_path):
     activate_file = open(activate_path, 'r')
     modified_activate_file = open(modified_activate_path, 'w')
     for line in activate_file:
-        if "$env:VIRTUAL_ENV=\"" in line:
-            line = "$env:VIRTUAL_ENV=\""+targetenv_path+'\"\n'
-        if "Write-Host -NoNewline -ForegroundColor Green \'(" in line:
-            line = "Write-Host -NoNewline -ForegroundColor Green \'(" + \
-                env_name+") \'\n"
+        if '$env:VIRTUAL_ENV=\"' in line:
+            line = '$env:VIRTUAL_ENV=\"'+targetenv_path+'\"\n'
+        if 'Write-Host -NoNewline -ForegroundColor Green \'(' in line:
+            line = 'Write-Host -NoNewline -ForegroundColor Green \'('+env_name+') \'\n'
         modified_activate_file.write(line)
     activate_file.close()
     modified_activate_file.close()
@@ -84,25 +85,21 @@ def fix_exe_interpreter(targetenv_path, modified_python_name):
             scriptexe_file = open(scriptexe_path, 'rb')
             rawcontent = scriptexe_file.read()
             if modified_python_name:
-                offset_python = offset_python = rawcontent.find(
-                    bytes(modified_python_name, 'ascii')+b'\x0a\x0d\x0aPK')
+                offset_python = offset_python = rawcontent.find(bytes(modified_python_name, 'ascii')+b'\x0a\x0d\x0aPK')
             else:
                 offset_python = rawcontent.find(b'python.exe\x0a\x0d\x0aPK')
             if offset_python == -1:
                 continue
             if modified_python_name:
                 end_offset = offset_python+len(modified_python_name)
-                replace_python_path = ('#!'+targetenv_path +
-                                       '\\Scripts\\'+modified_python_name).encode('ascii')
+                replace_python_path = ('#!'+targetenv_path+'\\Scripts\\'+modified_python_name).encode('ascii')
             else:
                 end_offset = offset_python+10
-                replace_python_path = ('#!'+targetenv_path +
-                                       '\\Scripts\\python.exe').encode('ascii')
+                replace_python_path = ('#!'+targetenv_path+'\\Scripts\\python.exe').encode('ascii')
             start_offset = offset_python
             while rawcontent[start_offset:start_offset+2] != b'#!':
                 start_offset -= 1
-            newcontent = rawcontent[:start_offset] + \
-                replace_python_path+rawcontent[end_offset:]
+            newcontent = rawcontent[:start_offset]+replace_python_path+rawcontent[end_offset:]
             scriptexe_file.close()
             modified_scriptexe_file = open(scriptexe_path+'.tmp', 'wb')
             modified_scriptexe_file.write(newcontent)
@@ -114,8 +111,7 @@ def fix_exe_interpreter(targetenv_path, modified_python_name):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 and len(sys.argv) != 3:
-        print('Usage: python '+sys.argv[0] +
-              ' <VENV_PATH> (modified_python_name)')
+        print('Usage: python '+sys.argv[0]+' <VENV_PATH> (modified_python_name)')
         exit()
     targetenv_path = sys.argv[1]
     targetenv_path = get_fullpath(targetenv_path)
