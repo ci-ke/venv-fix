20a7fe09ed930c2e0155bbf661ae9bcbd9d71e8a
cike <14360436+ci-ke@users.noreply.github.com>
2021-08-07 19:30:55 +0800

    add python 3.9 support

--- README.md
+++ README.md
@@ -4,16 +4,16 @@
 
 ## Usage: 
 
-python venvrelocate.py <VENV_PATH> (modified_python_name)
+python venvrelocate.py <new_venv_python_path>
 
 ## Example:
 
-After you move or rename virtual environment D:\venvs\venv1 to D:\venvs\venv2, the activate scripts and executable files in venv2\scripts should not work.
+After you move or rename virtual environment "D:\venvs\venv1" to "D:\venvs\venv2", the activate scripts and executable files in venv2\scripts (or venv2\bin) could not work.
 
-Then you can execute "python venvrelocate.py D:\venvs\venv2".
+Then you can execute "python venvrelocate.py D:\venvs\venv2\scripts\python.exe".
 
-And then venv2 will work properly, you can activate it and use pip as usual.
+And then venv2 will work properly, you can activate it, use pip and other executable files as usual.
 
 ## Notice:
 
-If there is a modified python name in the venv\scripts such as "python3.exe" or "python36.exe"... , you should also send this name to arguments. (e.g. python venvrelocate.py venv1 python36.exe).
+The <new_venv_python_path> can be absolute path or relative path of the python interpreter.
--- venvrelocate.py
+++ venvrelocate.py
@@ -2,32 +2,57 @@ import os
 import sys
 
 
-def get_fullpath(path):
-    if os.path.isdir(path):
-        dirs = os.listdir(path)
-        if 'Scripts' in dirs and 'Lib' in dirs:
-            fullpath = os.path.realpath(path)
-            return fullpath
-    print('path is not a virtualenv')
-    exit()
+def is_python_exe(python_path: str) -> bool:
+    with open(python_path, 'rb') as exe_file:
+        raw_content = exe_file.read()
+        offset_python = raw_content.find(b'.exe\x0a\x0d\x0aPK\x03\x04\x14\x00\x00\x00\x00\x00')
+    if offset_python == -1:
+        return True
+    else:
+        return False
 
 
-def fix_activate(targetenv_path):
-    env_name = targetenv_path.split('\\')[-1]
-    activate_path = targetenv_path+'\\Scripts\\activate'
-    modified_activate_path = targetenv_path+'\\Scripts\\activate.tmp'
+def parse_path(python_path: str) -> tuple:
+    if os.path.isfile(python_path) and is_python_exe(python_path):
+        full_python_path = os.path.abspath(python_path)
+        path_split = full_python_path.split('\\')
+
+        venv_path = '\\'.join(path_split[:-2])
+        python_folder_path = '\\'.join(path_split[:-1])
+        venv_name = path_split[-3]
+        python_name = path_split[-1]
+
+        files_in_venv = os.listdir(venv_path)
+        files_in_venv = [s.lower() for s in files_in_venv]
+        if 'lib' in files_in_venv and 'pyvenv.cfg' in files_in_venv:
+            return venv_path, python_folder_path, python_name, venv_name
+        else:
+            print('This path is not in a venv.')
+            exit()
+    else:
+        print('This is not a python interpreter name.')
+        exit()
+
+
+def fix_activate(venv_path, python_folder_path, python_name, venv_name) -> None:
+    activate_path = python_folder_path+'\\activate'
+    modified_activate_path = python_folder_path+'\\activate.tmp'
     if not os.path.exists(activate_path):
         return
     activate_file = open(activate_path, 'r')
     modified_activate_file = open(modified_activate_path, 'w')
+
     for line in activate_file:
-        if 'VIRTUAL_ENV=\"' in line:
-            line = 'VIRTUAL_ENV=\"'+targetenv_path+'\"\n'
-        if ') \" != x ] ; then' in line:
-            line = '    if [ \"x('+env_name+') \" != x ] ; then\n'
-        if ') ${PS1:-}\"' in line:
-            line = '    PS1=\"('+env_name+') ${PS1:-}\"\n'
+        if 'VIRTUAL_ENV=\"' in line:  # old file
+            line = 'VIRTUAL_ENV=\"'+venv_path+'\"\n'
+        if 'VIRTUAL_ENV=$(cygpath' in line:  # new file
+            line = 'VIRTUAL_ENV=$(cygpath \"'+venv_path+'\")\n'
+        if ') \" != x ] ; then' in line:  # old file
+            line = '    if [ \"x('+venv_name+') \" != x ] ; then\n'
+        if ') ${PS1:-}\"' in line:  # old and new file
+            line = '    PS1=\"('+venv_name+') ${PS1:-}\"\n'
         modified_activate_file.write(line)
+
     activate_file.close()
     modified_activate_file.close()
     os.remove(activate_path)
@@ -35,20 +60,21 @@ def fix_activate(targetenv_path):
     print(activate_path+' fixed')
 
 
-def fix_activate_bat(targetenv_path):
-    env_name = targetenv_path.split('\\')[-1]
-    activate_path = targetenv_path+'\\Scripts\\activate.bat'
-    modified_activate_path = targetenv_path+'\\Scripts\\activate.bat.tmp'
+def fix_activate_bat(venv_path, python_folder_path, python_name, venv_name) -> None:
+    activate_path = python_folder_path+'\\activate.bat'
+    modified_activate_path = python_folder_path+'\\activate.bat.tmp'
     if not os.path.exists(activate_path):
         return
     activate_file = open(activate_path, 'r')
     modified_activate_file = open(modified_activate_path, 'w')
+
     for line in activate_file:
         if 'set VIRTUAL_ENV=' in line:
-            line = 'set VIRTUAL_ENV='+targetenv_path+'\n'
+            line = 'set VIRTUAL_ENV='+venv_path+'\n'
         if 'set PROMPT=(' in line:
-            line = 'set PROMPT=('+env_name+') %PROMPT%\n'
+            line = 'set PROMPT=('+venv_name+') %PROMPT%\n'
         modified_activate_file.write(line)
+
     activate_file.close()
     modified_activate_file.close()
     os.remove(activate_path)
@@ -56,20 +82,21 @@ def fix_activate_bat(targetenv_path):
     print(activate_path+' fixed')
 
 
-def fix_activate_ps1(targetenv_path):
-    env_name = targetenv_path.split('\\')[-1]
-    activate_path = targetenv_path+'\\Scripts\\activate.ps1'
-    modified_activate_path = targetenv_path+'\\Scripts\\activate.ps1.tmp'
+def fix_activate_ps1(venv_path, python_folder_path, python_name, venv_name) -> None:
+    activate_path = python_folder_path+'\\activate.ps1'
+    modified_activate_path = python_folder_path+'\\activate.ps1.tmp'
     if not os.path.exists(activate_path):
         return
     activate_file = open(activate_path, 'r')
     modified_activate_file = open(modified_activate_path, 'w')
+
     for line in activate_file:
-        if '$env:VIRTUAL_ENV=\"' in line:
-            line = '$env:VIRTUAL_ENV=\"'+targetenv_path+'\"\n'
-        if 'Write-Host -NoNewline -ForegroundColor Green \'(' in line:
-            line = 'Write-Host -NoNewline -ForegroundColor Green \'('+env_name+') \'\n'
+        if '$env:VIRTUAL_ENV=\"' in line:  # old file
+            line = '$env:VIRTUAL_ENV=\"'+venv_path+'\"\n'
+        if 'Write-Host -NoNewline -ForegroundColor Green \'(' in line:  # old file
+            line = 'Write-Host -NoNewline -ForegroundColor Green \'('+venv_name+') \'\n'
         modified_activate_file.write(line)
+
     activate_file.close()
     modified_activate_file.close()
     os.remove(activate_path)
@@ -77,49 +104,41 @@ def fix_activate_ps1(targetenv_path):
     print(activate_path+' fixed')
 
 
-def fix_exe_interpreter(targetenv_path, modified_python_name):
-    scriptexe_names = os.listdir(targetenv_path+'\\Scripts')
-    for scriptexe_name in scriptexe_names:
-        if scriptexe_name.split('.')[-1] == 'exe' and scriptexe_name != 'python.exe' and scriptexe_name != 'pythonw.exe' and scriptexe_name != modified_python_name:
-            scriptexe_path = targetenv_path+'\\Scripts\\'+scriptexe_name
-            scriptexe_file = open(scriptexe_path, 'rb')
-            rawcontent = scriptexe_file.read()
-            if modified_python_name:
-                offset_python = offset_python = rawcontent.find(bytes(modified_python_name, 'ascii')+b'\x0a\x0d\x0aPK')
-            else:
-                offset_python = rawcontent.find(b'python.exe\x0a\x0d\x0aPK')
+def fix_exe_interpreter(venv_path, python_folder_path, python_name, venv_name) -> None:
+    file_names = os.listdir(python_folder_path)
+    for file_name in file_names:
+        if file_name.split('.')[-1] == 'exe':
+            file_path = python_folder_path+'\\'+file_name
+            exe_file = open(file_path, 'rb')
+
+            raw_content = exe_file.read()
+            offset_python = raw_content.find(b'.exe\x0a\x0d\x0aPK\x03\x04\x14\x00\x00\x00\x00\x00')
             if offset_python == -1:
                 continue
-            if modified_python_name:
-                end_offset = offset_python+len(modified_python_name)
-                replace_python_path = ('#!'+targetenv_path+'\\Scripts\\'+modified_python_name).encode('ascii')
-            else:
-                end_offset = offset_python+10
-                replace_python_path = ('#!'+targetenv_path+'\\Scripts\\python.exe').encode('ascii')
             start_offset = offset_python
-            while rawcontent[start_offset:start_offset+2] != b'#!':
+            while raw_content[start_offset:start_offset+2] != b'#!':
                 start_offset -= 1
-            newcontent = rawcontent[:start_offset]+replace_python_path+rawcontent[end_offset:]
-            scriptexe_file.close()
-            modified_scriptexe_file = open(scriptexe_path+'.tmp', 'wb')
-            modified_scriptexe_file.write(newcontent)
-            modified_scriptexe_file.close()
-            os.remove(scriptexe_path)
-            os.rename(scriptexe_path+'.tmp', scriptexe_path)
-            print(scriptexe_path+' fixed')
+            end_offset = offset_python+4
+
+            replace_python_path = ('#!'+python_folder_path+'\\'+python_name).encode('ascii')
+            new_content = raw_content[:start_offset]+replace_python_path+raw_content[end_offset:]
+
+            exe_file.close()
+            modified_exe_file = open(file_path+'.tmp', 'wb')
+            modified_exe_file.write(new_content)
+            modified_exe_file.close()
+            os.remove(file_path)
+            os.rename(file_path+'.tmp', file_path)
+            print(file_path+' fixed')
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 and len(sys.argv) != 3:
-        print('Usage: python '+sys.argv[0]+' <VENV_PATH> (modified_python_name)')
+    if len(sys.argv) != 2:
+        print('Usage: python '+sys.argv[0]+' <new_venv_python_path>')
         exit()
-    targetenv_path = sys.argv[1]
-    targetenv_path = get_fullpath(targetenv_path)
-    if len(sys.argv) == 3:
-        modified_python_name = sys.argv[2]
-    else:
-        modified_python_name = None
-    fix_activate(targetenv_path)
-    fix_activate_bat(targetenv_path)
-    fix_activate_ps1(targetenv_path)
-    fix_exe_interpreter(targetenv_path, modified_python_name)
+    python_path = sys.argv[1]
+    ret = parse_path(python_path)
+    fix_activate(*ret)
+    fix_activate_bat(*ret)
+    fix_activate_ps1(*ret)
+    fix_exe_interpreter(*ret)
